b64f65fbb3da8a57eb8f909e6fc676668c7fbec7
Chitransh6 <chitranshsharma067@gmail.com>
2025-12-25 09:30:37 +0530

    first class

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,23 @@
 function App() {
   
   return (
-    <h1>hello world</h1>
+    <h1> 1. it is my first of react and i learned how to create react project by using vite bundler
+      <br />
+     2. how to push code in github <br />
+     3. package.json stores the dependencies that we need to made our project,it store approximate virsion of dependencies <br />
+     4. pakage-lock.json stores the exact version of dependencies <br />
+     
+     5. put files in gitignore that we does not want to push in github <br />
+     
+     6. node_modules is like a database for dependencies project dependencies need their dependencies that are stored in node_modules <br />
+
+     7. in package.json dependencies has ^(carot) and ~(tilde) <br />
+     ~ = it represents major updation in dependencies <br />
+     ^ = it represents minor updation in dependencies <br />
+
+     8. readme.md basicaly use like a note pad for project
+     </h1>
+
      
   )
 }
